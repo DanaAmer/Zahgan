25e8df43e166591a196de42af53596f9ff4e9b77
DanaAmer <3amer.dana@gmail.com>
2018-12-13 16:36:11 +0200

    <fix> fix the help modal

--- react-client/src/components/about/Vision.js
+++ react-client/src/components/about/Vision.js
@@ -63,7 +63,7 @@ class Vision extends React.Component {
         queryDetails
       },
       success: data => {
-        alert(data.name);
+        alert("Your feedback has been sent successfully");
         $("input, textarea").val("");
       },
       error: err => {
@@ -87,8 +87,7 @@ class Vision extends React.Component {
   }
 
   render() {
-    return (
-      <div id="about">
+    return <div id="about">
         <div className="row">
           <div className="col-md-12">
             <p className="main_title">
@@ -107,8 +106,7 @@ class Vision extends React.Component {
                 <h4 className="text-center">About</h4>
                 <p>
                   {" "}
-                  Our mission is to provide you with a tool to find the best
-                  events happening around you <br />
+                  Our mission is to provide you with a tool to find the best events happening around you <br />
                   <u>read more</u>
                 </p>
               </div>
@@ -141,22 +139,14 @@ class Vision extends React.Component {
                 <h4 className="text-center">help</h4>
                 <p>
                   {" "}
-                  Do you need help? Our team will be happy to help you, contact
-                  us at <br /> 06-xxxxxxx{" "}
+                  Do you need help? Our team will be happy to help you, contact us at <br /> 06-xxxxxxx{" "}
                 </p>{" "}
               </div>
             </div>
           </div>
         </div>
         <div>
-          <Modal
-            className={"modal-open"}
-            isOpen={this.state.modal}
-            toggle={this.toggle}
-            centered={true}
-            size={"lg"}
-            style={{ marginTop: "100px" }}
-          >
+          <Modal className={"modal-open"} isOpen={this.state.modal} toggle={this.toggle} centered={true} size={"lg"} style={{ marginTop: "100px" }}>
             <ModalBody>
               <h2
                 style={{
@@ -167,83 +157,59 @@ class Vision extends React.Component {
                 }}
               >
                 About Us
-              </h2>{" "}
-              <hr />
+              </h2> <hr />
               <p style={{ fontSize: "20px", fontFamily: "Times New Roman" }}>
                 {" "}
-                <strong>Zahgaan</strong> is an online event registration created
-                to help manage events, festivals, conferences, workshops and
-                activities. it saves you the time and effort of managing events,
-                handling attendee communication, manual registration.
+                <strong>
+                  Zahgaan
+                </strong> is an online event registration created to help manage events, festivals, conferences, workshops and activities. it saves you the time and effort of managing events, handling attendee communication, manual registration.
                 <br /> <br />
-                Zahgaan.com is a service platform that allows event organizers
-                or venue managers to post their events and start accepting
-                registrations or reservations online, in addition to that,
-                Zahgaan.com provides its audiences with the latest events and
-                alerts based on their personal preferences.
+                Zahgaan.com is a service platform that allows event organizers or venue managers to post their events and start accepting registrations or reservations online, in addition to that, Zahgaan.com provides its audiences with the latest events and alerts based on their personal preferences.
               </p>
             </ModalBody>
           </Modal>
-          <Modal
-            className={"modal-open"}
-            isOpen={this.state.modal1}
-            toggle={this.toggle1}
-            centered={true}
-            size={"lg"}
-            style={{ marginTop: "150px" }}
-          >
+          <Modal className={"modal-open"} isOpen={this.state.modal1} toggle={this.toggle1} centered={true} size={"lg"} style={{ marginTop: "150px" }}>
             <ModalBody>
               <div className="location">
                 <main className=" m-0 p-0">
                   <div className="z-depth-1-half map-container-4 map-container-google-4 ">
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3384.051567103805!2d35.83549401509962!3d31.986617531171028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ca18e39fe576b%3A0xf308701edd298519!2sRBK+Hacker+Haus!5e0!3m2!1sen!2sjo!4v1543954476718"
-                      allowfullscreen
-                    />
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3384.051567103805!2d35.83549401509962!3d31.986617531171028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ca18e39fe576b%3A0xf308701edd298519!2sRBK+Hacker+Haus!5e0!3m2!1sen!2sjo!4v1543954476718" allowfullscreen />
                   </div>
                 </main>
               </div>
             </ModalBody>
           </Modal>
-          <Modal
-            className={"modal-open"}
-            isOpen={this.state.Help}
-            toggle={this.toggleHelp}
-            centered={true}
-            size={"lg"}
-            style={{ marginTop: "150px" }}
-          >
-            <ModalBody>
+          <Modal className={"modal-open"} isOpen={this.state.Help} toggle={this.toggleHelp} centered={true} size={"lg"} style={{ marginTop: "150px", fontSize: "20px" }}>
+          <ModalBody style={{ width: "600px" }}>
               <Form style={{ width: "600px" }}>
                 <FormGroup>
+                  <h2 style={{ textAlign: "center", fontWeight: "bold" }}>
+                    {" "}
+                    Write to us
+                  </h2>
                   <Label for="name">Name</Label>
                   <Input type="text" name="name" onChange={this.handleName} />
                 </FormGroup>
 
                 <FormGroup>
                   <Label for="email">E-mail</Label>
-                  <Input
-                    type="email"
-                    name="email"
-                    onChange={this.handleEmail}
-                  />
+                  <Input type="email" name="email" onChange={this.handleEmail} />
                 </FormGroup>
 
                 <FormGroup>
                   <Label for="message">Message</Label>
-                  <Input
-                    type="textarea"
-                    name="message"
-                    onChange={this.handleMessage}
-                  />
+                  <Input type="textarea" name="message" onChange={this.handleMessage} />
                 </FormGroup>
-                <Button onClick={this.handleSubmitHelp}>Submit</Button>
+              <Button onClick={this.handleSubmitHelp}>Send</Button>
               </Form>
             </ModalBody>
           </Modal>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 export default Vision;
+
+
+
+
